cb772bcf5f5a2e415cd2e55fe0815d4474b9db25
princemor71 <princemoradiya7175@gmail.com>
2025-01-05 12:00:02 +0530

    Add files via upload

--- dflipflop.v
+++ dflipflop.v
@@ -3,7 +3,7 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 04.01.2025 23:38:19
+// Create Date: 04.01.2025 23:49:12
 // Design Name: 
 // Module Name: dflipflop
 // Project Name: 
@@ -18,3 +18,45 @@
 // Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+module dflipflop(j,k,q,qn,clk);
+//input d,clk;
+//output q,qn;
+//reg q,qn;
+//initial
+//begin q=1;qn=0 ; end
+//always @(posedge clk)
+//begin
+//case ({d})
+//1'b0: begin q=0;qn=1; end
+//1'b1: begin q=1; qn=0; end 
+//endcase
+//end
+input j,k, clk;
+output q,qn;
+reg q,qn;
+initial
+begin q=1;qn=0;end
+always @(posedge clk)
+case({j,k})
+2'b00:begin q=1;qn=0; end
+2'b01: begin q=0; qn=1; end
+2'b10 : begin q=1 ; qn=0; end 
+2'b11 : begin q=~q; qn=~qn ; end 
+endcase 
+
+endmodule
+
+module jkflip (j,k,q,qn,clk);
+output [3:0]q,qn;
+input j,k,clk;
+wire [3:0]q;
+
+dflipflop l0(q[0],qn[0],j,k,clk);
+dflipflop l1(q[1],qn[1],j,k,clk);
+dflipflop l2(q[2],qn[2],j,k,clk);
+dflipflop l3(q[3],qn[3],j,k,clk);
+endmodule
